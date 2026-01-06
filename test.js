9848fee52afa63c6fd5b3cd73dc498428f21708c
kazirobinking <kazirobinking@gmail.com>
2026-01-06 06:47:27 +0600

    Refactor news list structure and styles for improved layout and scrolling behavior

--- test.js
+++ test.js
@@ -1,15 +1,16 @@
-// A simple function
-function showName(name) {
-    console.log("Name:", name);
+// let names = "Robin";
+
+// names = "js"
+// let user = names
+// user = "dev"
+// console.log(user) //dev
+// // console.log(names) //Robin
+
+const users ={
+    name: "Robin"
 }
 
-
-// Higher-order function: accepts another function as a parameter
-function withAge(fn, age) {
-    // Call the function passed in (fn) with a name
-    fn("Alice");
-    console.log("Age:", age);
-}
-
-// Pass showName as an argument to withAge
-withAge(showName, 25);
+const user2 = users
+user2.name = "dev"
+// console.log(user2.name) 
+console.log(users.name) //Robin
